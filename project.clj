81291ca324acfb271f49096d90fa492398b3d0ba
Daniel Vieira <daniel.vieira@flexiana.gov.br>
2021-12-29 16:55:14 -0300

    upgrade http lib

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :main notion-to-md.core
   :dependencies [[org.clojure/data.json "0.2.6"]
                  [environ "1.0.0"]
-                 [clj-http "3.10.1"]
+                 [clj-http "3.12.3"]
                  [org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns notion-to-md.core})
