f3f2bb6be162ed7374ab9749f61a70eb4e60d033
Daniel Vieira <daniel.vieira@flexiana.gov.br>
2022-01-05 11:35:47 -0300

    version 0.1.3 fixes parsing "mentions"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.flexiana/notion-to-md "0.1.2"
+(defproject com.flexiana/notion-to-md "0.1.3"
   :description "Notion To Markdown converter"
   :uberjar-name "notion-to-md.jar"
   :test-paths ["test"]
@@ -10,7 +10,6 @@
                  [clj-http "3.12.3"]
                  [org.clojure/clojure "1.10.0"]]
   :profiles {:local
-             {:dependencies [[clj-http "3.12.3"]
-                             [com.flexiana/notion-to-md "0.1.2"]]}}
+             {:dependencies [[com.flexiana/notion-to-md "0.1.3"]]}}
   :aliases {"notion-to-md"  ["with-profile" "local" "run" "-m" "notion-to-md.core"]}
   :repl-options {:init-ns notion-to-md.core})
